7a1a3b6afb61d3b14da1fe56605c974ca2689377
Alex Simpson <gkrabsti@gmail.com>
2022-03-27 00:03:33 +0200

    made changes in main.py and in characterGen.py adding generation options in main.py

--- characterGen.py
+++ characterGen.py
@@ -3,25 +3,25 @@ import random
 import discord
 
 
-sexes = ('М', 'Ж')
+sexes = (':male_sign: М', ':female_sign: Ж')
 
-cataclysms=()
+cataclysms = ()
 profs = ()
-phobias = ()  
-hobbies = () 
-cards = () 
+phobias = ()
+hobbies = ()
+cards = ()
 
 cataclysmsFile = open('cataclysms.txt', 'r', encoding="utf-8")
 for i in cataclysmsFile:
     cataclysms = i.split(",")
 cataclysmsFile.close()
 
-profsFile = open('proffession.txt', 'r', encoding="utf-8") 
+profsFile = open('proffession.txt', 'r', encoding="utf-8")
 for i in profsFile:
     profs = i.split(",")
-profsFile.close() 
+profsFile.close()
 
-phobiasFile = open('phobias.txt', 'r', encoding="utf-8") 
+phobiasFile = open('phobias.txt', 'r', encoding="utf-8")
 for i in phobiasFile:
     phobias = i.split("$")
 phobiasFile.close()
@@ -36,12 +36,17 @@ for i in cardsFile:
     cards = i.split("$")
 cardsFile.close()
 
-bodyTypes = ("Не может самостоятельно передвигаться из-за ожирения", "Не хватает сил, чтобы перемещаться самостоятельно", "Хилое", "Худое", "Стройное", "Мускулистое", "Полное", "Жирное", "Геракл", "Афина")
-bodyLimbs = ("всё при себе", "нет руки", "нет ноги", "нет рук", "нет ног", "Вуйчич", "нет глаза", "нет уха")
+bodyTypes = ("Не может самостоятельно передвигаться из-за ожирения", "Не хватает сил, чтобы перемещаться самостоятельно",
+             "Хилое", "Худое", "Стройное", "Мускулистое", "Полное", "Жирное", "Геракл", "Афина")
+bodyLimbs = ("всё при себе", "нет руки", "нет ноги", "нет рук",
+             "нет ног", "Вуйчич", "нет глаза", "нет уха")
 
-health = ("Здоров", "HIV", "Герпес", "Геморрой", "Наркоман", "Наркоман крайней степени тяжести", "Глухонемой", "Глухой", "Немой", "Маниакальное расстройство", "Паранойя", "Шизофрения", "Астма", "Астма в крайней степени тяжести", "Рак легких", "Фимоз", "Радикулит", "Остеопороз", "Идеально здоров", "Простуда", "Хронический бронхит", "Хронический насморк", "Пневмония", "В целом здоров", "Кариес", "Флюс", "Артрит", "Гипертония", "Гипотония", "Расстройство пищевого поведения")
+health = ("Здоров", "HIV", "Герпес", "Геморрой", "Наркоман", "Наркоман крайней степени тяжести", "Глухонемой", "Глухой", "Немой", "Маниакальное расстройство", "Паранойя", "Шизофрения", "Астма", "Астма в крайней степени тяжести", "Рак легких",
+          "Фимоз", "Радикулит", "Остеопороз", "Идеально здоров", "Простуда", "Хронический бронхит", "Хронический насморк", "Пневмония", "В целом здоров", "Кариес", "Флюс", "Артрит", "Гипертония", "Гипотония", "Расстройство пищевого поведения")
 
-traits = ("Мерзкий", "Скромный", "Беззрасудный", "Гений", "Маэстро", "Хладнокровный", "Никакущий", "Мечтатель", "Подлый", "Добрый", "Любезный", "Хороший", "Среднестатист", "Человечишка", "Быдло", "Жалкий", "Крутой", "Скромный", "Глупый", "Обаяшка", "Диоген")
+traits = ("Мерзкий", "Скромный", "Беззрасудный", "Гений", "Маэстро", "Хладнокровный", "Никакущий", "Мечтатель", "Подлый", "Добрый",
+          "Любезный", "Хороший", "Среднестатист", "Человечишка", "Быдло", "Жалкий", "Крутой", "Скромный", "Глупый", "Обаяшка", "Диоген")
+
 
 def characterCreation():
     createdCharacter = ''
@@ -54,8 +59,8 @@ def characterCreation():
     else:
         bodyCondition = (random.choice(bodyTypes) + f', {bCL}')
 
-    proffesion = str(random.choice(profs)) 
-    profLevel = random.randint(1, 5)
+    proffesion = str(random.choice(profs))
+    profLevel = random.randrange(0, 5)
     healthCondition = str(random.choice(health))
     hobbie = str(random.choice(hobbies))
     phobia = str(random.choice(phobias))
@@ -63,15 +68,25 @@ def characterCreation():
     card1 = str(random.choice(cards))
     card2 = str(random.choice(cards))
 
-    while (card1==card2): # no duplicate card
-        card2=random.choice(cards)
+    while (card1 == card2):  # no duplicate card
+        card2 = random.choice(cards)
 
-    createdCharacterPool = (f'Пол: {sex}', f'Возраст: {str(age)}', f'Тело: {bodyCondition.lower()}', f'Проффессия: {proffesion.lower()}, умение: {str(profLevel)} из 5', f'Здоровье: {healthCondition.lower()}', f'Хобби: {hobbie.lower()}', f'Фобия: {phobia.lower()}', f'Характер: {trait.lower()}', f'Карта №1: {card1.lower()}', f'Карта №2: {card2.lower()}')
+    createdCharacterPool = [
+        f':busts_in_silhouette: Пол: {sex}',
+        f':calendar_spiral: Возраст: {str(age)}',
+        f':bust_in_silhouette: Тело: {bodyCondition.lower()}',
+        f':tools: Проффессия: {proffesion.lower()}, умение: {str(profLevel)} из 5',
+        f':anatomical_heart: Здоровье: {healthCondition.lower()}',
+        f':thought_balloon: Хобби: {hobbie.lower()}',
+        f':eye: Фобия: {phobia.lower()}',
+        f':speaking_head: Характер: {trait.lower()}\n',
+        f':flower_playing_cards: №1: {card1.lower()}',
+        f':flower_playing_cards: №2: {card2.lower()}']
 
     for i in createdCharacterPool:
         createdCharacter = createdCharacter + i + '\n'
 
     return createdCharacter
-    
 
-print(characterCreation())
+
+#print(characterCreation())
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from multiprocessing.connection import wait
+from time import sleep
 import discord
 import characterGen
 
@@ -7,18 +9,31 @@ client = discord.Client()
 async def on_ready():
     print('logged in as {0.user}'.format(client))
 
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    
-    if message.content.startswith('$id'):
-        name = message.author
-        await message.channel.send('твой id: {0}'.format(name))
+        
+    #if message.content.startswith('$id'):
+    #    name = message.author
+    #    await message.channel.send('твой id: {0}'.format(name))
 
     if message.content.startswith('$ген'):
-        await message.author.send(characterGen.characterCreation())
-    
+        await message.author.send(f'Твой персонаж, {message.author}!\n–----------------–\n' + characterGen.characterCreation() + '\n–----------------–\n')
+
+    if message.content.startswith('$ген 3'):
+        await message.author.send(f'Твои персонажи, {message.author}!\n№1\n–----------------–\n' + characterGen.characterCreation() + '\n–----------------–\n')
+        await message.author.send('\n№2\n–----------------–\n' + characterGen.characterCreation() + '\n–----------------–\n')
+        await message.author.send('\n№3\n–----------------–\n' + characterGen.characterCreation() + '\n–----------------–\n')
+
+    if message.content.startswith('$ген 10'):
+        await message.author.send(f'Твои персонажи, {message.author}!\n№1\n–----------------–\n' + characterGen.characterCreation() + '\n–----------------–\n')
+        for i in range(2, 11):
+            sleep(0.5)
+            await message.author.send(f'\n№{i}\n–----------------–\n' + characterGen.characterCreation() + '\n–----------------–\n')
+            
+
     if message.content.startswith('$hello'):
         await message.channel.send('дарова')
 
